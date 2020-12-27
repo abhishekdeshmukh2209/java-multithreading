089a9255ba160de1e75ff68ab6f789b199b9f95d
Abhishek Shyam Deshmukh <abhishekdeshmukh.com@gmail.com>
2020-12-28 01:45:28 +0530

    Added comment in main class

--- multithreaded-server/src/main/java/com/abhishek/assignment/customthreadpool/threadpools/MultithreadedServer.java
+++ multithreaded-server/src/main/java/com/abhishek/assignment/customthreadpool/threadpools/MultithreadedServer.java
@@ -15,8 +15,7 @@ import java.util.Arrays;
 import java.util.List;
 import com.abhishek.assignment.customthreadpool.threadpools.ThreadPool;
 
-
-
+//Main Class
 public class MultithreadedServer {
 
 	public static void main(String[] args) {
